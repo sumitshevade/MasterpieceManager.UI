0c85c616a4d2fbaa2a09e62605b9263e8d576ace
sumitshevade <sumitshevade@gmail.com>
2021-10-16 08:09:38 +0530

    Removed unwanted links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,10 +26,6 @@ import { AppComponent } from 'app/app.component';
 import { LayoutModule } from 'app/layout/layout.module';
 import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
 
-import { ContextMenuComponent } from 'app/main/extensions/context-menu/context-menu.component';
-import { AnimatedCustomContextMenuComponent } from './main/extensions/context-menu/custom-context-menu/animated-custom-context-menu/animated-custom-context-menu.component';
-import { BasicCustomContextMenuComponent } from './main/extensions/context-menu/custom-context-menu/basic-custom-context-menu/basic-custom-context-menu.component';
-import { SubMenuCustomContextMenuComponent } from './main/extensions/context-menu/custom-context-menu/sub-menu-custom-context-menu/sub-menu-custom-context-menu.component';
 import { ContactsModule } from './mpm/contacts/contacts.module';
 import { ApiModule } from 'swagger/providers/api.module';
 
@@ -39,51 +35,12 @@ const appRoutes: Routes = [
     loadChildren: () => import('./mpm/contacts/contacts.module').then(m => m.ContactsModule)
   },
   {
-    path: 'dashboard',
-    loadChildren: () => import('./main/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
-    path: 'apps',
-    loadChildren: () => import('./main/apps/apps.module').then(m => m.AppsModule),
-    canActivate: [AuthGuard]
-  },
-  {
     path: 'pages',
     loadChildren: () => import('./main/pages/pages.module').then(m => m.PagesModule)
   },
   {
-    path: 'ui',
-    loadChildren: () => import('./main/ui/ui.module').then(m => m.UIModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'components',
-    loadChildren: () => import('./main/components/components.module').then(m => m.ComponentsModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'extensions',
-    loadChildren: () => import('./main/extensions/extensions.module').then(m => m.ExtensionsModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'forms',
-    loadChildren: () => import('./main/forms/forms.module').then(m => m.FormsModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'tables',
-    loadChildren: () => import('./main/tables/tables.module').then(m => m.TablesModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'charts-and-maps',
-    loadChildren: () => import('./main/charts-and-maps/charts-and-maps.module').then(m => m.ChartsAndMapsModule),
-    canActivate: [AuthGuard]
-  },
-  {
     path: '',
-    redirectTo: '/dashboard/ecommerce',
+    redirectTo: '/contacts/contact',
     pathMatch: 'full'
   },
   {
@@ -94,11 +51,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContextMenuComponent,
-    BasicCustomContextMenuComponent,
-    AnimatedCustomContextMenuComponent,
-    SubMenuCustomContextMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -134,7 +87,7 @@ const appRoutes: Routes = [
     // ! IMPORTANT: Provider used to create fake backend, comment while using real API
     fakeBackendProvider
   ],
-  entryComponents: [BasicCustomContextMenuComponent, AnimatedCustomContextMenuComponent],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
